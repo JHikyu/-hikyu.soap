aae9d4150b3c20ca451a6756a43cba5b2e612608
Justin <therosti@gmail.com>
2022-02-04 21:18:38 +0100

    Added render event

--- examples/website with pug/index.js
+++ examples/website with pug/index.js
@@ -3,6 +3,7 @@ const soap = require('@hikyu/soap');
 //* Event listeners
 soap.once('started', (data) => console.log(`Server started on port ${data.port}`));
 soap.on('get', (data) => console.log(`GET ${data.path}`));
+soap.on('render', (data) => console.log(`Rendering ${data.path}`));
 
 //* Routes
 soap.get('/', (req, res) => res.render('landing.pug', { title: 'Soap 🧼' }));
